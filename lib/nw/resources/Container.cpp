85bc5e70d8916c808e94c5f21f5f9bb3a5194cd2
jmd <joshua.m.dean@gmail.com>
2021-07-31 17:51:53 -0700

    resources: Add a very basic Container class
    This is the class that all containers will inherit from.

---
+++ lib/nw/resources/Container.cpp
@@ -0,0 +1,5 @@
+#include "Container.hpp"
+
+namespace nw {
+
+} // namespace nw
---
+++ lib/nw/resources/Container.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../util/ByteArray.hpp"
+#include "Resource.hpp"
+
+#include <filesystem>
+
+namespace nw {
+
+/**
+ * @brief Base class for all containers.
+ *
+ */
+struct Container {
+    virtual ~Container() = default;
+
+    /**
+     * @brief Read resourece data
+     *
+     * @param res Qualified resource name
+     * @return ByteArray If the container does not hold the resource, the resulting ``nw::ByteArray`` will be of size 0.
+     */
+    virtual ByteArray demand(Resource res) = 0;
+
+    virtual int extract(std::string_view glob, const std::filesystem::path& output) = 0;
+    virtual int extract(const std::regex& pattern, const std::filesystem::path& output) = 0;
+
+    /**
+     * @brief Determines the size, if applicable, of the container
+     *
+     * @return size_t
+     */
+    virtual size_t size() const { return 0; }
+};
+
+} // namespace nw
