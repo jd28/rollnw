9806edca837d0c659a28090435e89489a98902be
jmd <joshua.m.dean@gmail.com>
2021-08-29 20:06:20 -0700

    util: Add scope_exit

---
+++ lib/nw/util/scope_exit.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace nw {
+
+// Remove if ever added to standard.
+
+template <typename T>
+class scope_exit {
+public:
+    explicit scope_exit(T&& exit)
+        : exit_(std::forward<T>(exit))
+    {
+    }
+
+    scope_exit(const scope_exit&) = delete;
+
+    ~scope_exit()
+    {
+        try {
+            exit_();
+        } catch (...) {
+        }
+    }
+
+    scope_exit& operator=(const scope_exit&) = delete;
+
+private:
+    T exit_;
+};
+
+template <typename T>
+scope_exit<T> create_scope_exit(T&& exit)
+{
+    return scope_exit<T>(std::forward<T>(exit));
+}
+
+} // namespace nw
+
+#define _EXIT_SCOPE_LINENAME_CAT(name, line) name##line
+#define _EXIT_SCOPE_LINENAME(name, line) _EXIT_SCOPE_LINENAME_CAT(name, line)
+#define SCOPE_EXIT(f) const auto& _EXIT_SCOPE_LINENAME(EXIT, __LINE__) = create_scope_exit(f)
