8aa2c95514635d46fdf75bfdb921e4d4961ac710
jmd <joshua.m.dean@gmail.com>
2021-10-17 16:28:48 -0700

    objects/components: Add very  basic combat info

---
+++ lib/nw/objects/components/Combat.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../Serialization.hpp"
+#include "SpellBook.hpp"
+
+namespace nw {
+
+struct SpecialAbility {
+    uint16_t spell;
+    uint8_t level;
+    SpellFlags flags = SpellFlags::none;
+};
+
+struct Combat {
+    Combat(const GffStruct gff);
+
+    uint8_t ac_natural = 0;
+    std::vector<SpecialAbility> special_abilities;
+};
+
+} // namespace nw
