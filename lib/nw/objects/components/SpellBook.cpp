a3f8b0f6b82c8f659d25aa33a531545b2e8e5904
jmd <joshua.m.dean@gmail.com>
2021-10-18 17:01:49 -0700

    objects/components: Add SpellBook

---
+++ lib/nw/objects/components/SpellBook.cpp
@@ -0,0 +1,30 @@
+#include "SpellBook.hpp"
+
+namespace nw {
+
+SpellBook::SpellBook(const GffStruct gff)
+{
+    for (size_t i = 0; i < 10; ++i) {
+        auto m = fmt::format("MemorizedList{}", i);
+        size_t sz = gff[m].size();
+        for (size_t j = 0; j < sz; ++j) {
+            Spell s;
+            gff[m][j].get_to("Spell", s.spell);
+            gff[m][j].get_to("SpellFlags", s.flags);
+            gff[m][j].get_to("SpellMetaMagic", s.meta);
+            memorized[i].push_back(s);
+        }
+
+        auto k = fmt::format("KnownList{}", i);
+        sz = gff[k].size();
+        for (size_t j = 0; j < sz; ++j) {
+            Spell s;
+            gff[k][j].get_to("Spell", s.spell);
+            gff[k][j].get_to("SpellFlags", s.flags);
+            gff[k][j].get_to("SpellMetaMagic", s.meta);
+            known[i].push_back(s);
+        }
+    }
+}
+
+} // namespace nw
---
+++ lib/nw/objects/components/SpellBook.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "../Serialization.hpp"
+
+#include <array>
+#include <cstdint>
+#include <vector>
+
+namespace nw {
+
+enum struct SpellFlags : uint8_t {
+    none = 0x0,
+    readied = 0x01,
+    spontaneouss = 0x02,
+    unlimited = 0x04,
+};
+
+enum struct SpellMetaMagic : uint8_t {
+    none = 0x00,
+    empower = 0x01,
+    extend = 0x02,
+    maximize = 0x04,
+    quicken = 0x08,
+    silent = 0x10,
+    still = 0x20,
+};
+
+struct Spell {
+    uint16_t spell;
+    SpellMetaMagic meta = SpellMetaMagic::none;
+    SpellFlags flags = SpellFlags::none;
+};
+
+struct SpellBook {
+    SpellBook() = default;
+    SpellBook(const GffStruct gff);
+
+    std::array<std::vector<Spell>, 9> known;
+    std::array<std::vector<Spell>, 9> memorized;
+};
+
+} // namespace nw
