762d76078fe5d85a1ae8cbb3a52eb8f91c687bcc
jmd <joshua.m.dean@gmail.com>
2021-12-17 18:05:59 -0800

    objects/components/Trap: Update to new (de)serialization style

---
+++ lib/nw/objects/components/Trap.cpp
@@ -0,0 +1,58 @@
+#include "Trap.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace nw {
+
+bool Trap::from_gff(const GffInputArchiveStruct& archive)
+{
+    if (!archive.get_to("TrapFlag", is_trapped)) {
+        LOG_F(ERROR, "attempting to read a trap from non-trappable object");
+        return false;
+    }
+    if (is_trapped) {
+        archive.get_to("TrapType", type);
+        archive.get_to("DisarmDC", disarm_dc);
+        archive.get_to("TrapDetectable", detectable);
+        archive.get_to("TrapDetectDC", detect_dc);
+        archive.get_to("TrapDisarmable", disarmable);
+        archive.get_to("TrapOneShot", one_shot);
+    }
+
+    return true;
+}
+
+bool Trap::from_json(const nlohmann::json& archive)
+{
+    try {
+        archive.at("is_trapped").get_to(is_trapped);
+        archive.at("type").get_to(type);
+        archive.at("detectable").get_to(detectable);
+        archive.at("detect_dc").get_to(detect_dc);
+        archive.at("disarmable").get_to(disarmable);
+        archive.at("disarm_dc").get_to(disarm_dc);
+        archive.at("one_shot").get_to(one_shot);
+    } catch (nlohmann::json::exception& e) {
+        LOG_F(ERROR, "Trap::to_json exception: {}", e.what());
+        return false;
+    }
+
+    return true;
+}
+
+nlohmann::json Trap::to_json() const
+{
+    nlohmann::json j;
+
+    j["is_trapped"] = is_trapped;
+    j["type"] = type;
+    j["detectable"] = detectable;
+    j["detect_dc"] = detect_dc;
+    j["disarmable"] = disarmable;
+    j["disarm_dc"] = disarm_dc;
+    j["one_shot"] = one_shot;
+
+    return j;
+}
+
+} // namespace nw
