46f2580d8fe56888d95271d5b891d0ea7b69845e
jmd <joshua.m.dean@gmail.com>
2021-12-18 11:58:09 -0800

    objects/components/Inventory: Update to new (de)serialization style

--- lib/nw/objects/components/Inventory.cpp
+++ lib/nw/objects/components/Inventory.cpp
@@ -2,14 +2,16 @@
 
 #include "../Item.hpp"
 
+#include <nlohmann/json.hpp>
+
 namespace nw {
 
-Inventory::Inventory(const GffInputArchiveStruct gff, SerializationProfile profile)
+bool Inventory::from_gff(const GffInputArchiveStruct& archive, SerializationProfile profile)
 {
-    size_t sz = gff["ItemList"].size();
+    size_t sz = archive["ItemList"].size();
     items.reserve(sz);
     for (size_t i = 0; i < sz; ++i) {
-        auto st = gff["ItemList"][i];
+        auto st = archive["ItemList"][i];
         InventoryItem ii;
 
         st.get_to("Repos_PosX", ii.pos_x);
@@ -25,6 +27,58 @@ Inventory::Inventory(const GffInputArchiveStruct gff, SerializationProfile profi
 
         items.push_back(std::move(ii));
     }
+
+    return true;
+}
+
+bool Inventory::from_json(const nlohmann::json& archive, SerializationProfile profile)
+{
+    if (!archive.is_array()) { return false; }
+
+    try {
+        items.reserve(archive.size());
+        for (size_t i = 0; i < archive.size(); ++i) {
+            InventoryItem ii;
+            archive[i].at("position")[0].get_to(ii.pos_x);
+            archive[i].at("position")[1].get_to(ii.pos_y);
+            if (profile == SerializationProfile::blueprint) {
+                ii.item = archive[i].at("item").get<Resref>();
+            } else {
+                ii.item = std::make_unique<Item>(archive[i].at("item"), profile);
+            }
+            items.push_back(std::move(ii));
+        }
+    } catch (const nlohmann::json::exception& e) {
+        LOG_F(ERROR, "Inventory::from_json exception: {}", e.what());
+        return false;
+    }
+
+    return true;
+}
+
+nlohmann::json Inventory::to_json(SerializationProfile profile) const
+{
+    nlohmann::json j = nlohmann::json::array();
+
+    try {
+        for (const auto& it : items) {
+            j.push_back({});
+            auto& payload = j.back();
+            payload["position"] = {it.pos_x, it.pos_y};
+            if (std::holds_alternative<std::unique_ptr<Item>>(it.item)) {
+                payload["item"] = std::get<std::unique_ptr<Item>>(it.item)->to_json(profile);
+            } else {
+                if (SerializationProfile::blueprint == profile) {
+                    payload["item"] = std::get<Resref>(it.item);
+                }
+            }
+        }
+    } catch (const nlohmann::json::exception& e) {
+        LOG_F(ERROR, "inventory::to_json exception: {}", e.what());
+        j.clear();
+    }
+
+    return j;
 }
 
 } // namespace nw
