53b913348452726ff71eafc169bcc2416a7fac56
jmd <joshua.m.dean@gmail.com>
2022-01-01 17:30:40 -0800

    util/templates: Add to_underlying for enums
    Can't wait for C++23

--- lib/nw/util/templates.hpp
+++ lib/nw/util/templates.hpp
@@ -5,4 +5,12 @@ namespace nw {
 template <typename T>
 constexpr bool always_false() { return false; }
 
+// Replace when C++23 comes around
+template <class Enum>
+constexpr std::underlying_type_t<Enum> to_underlying(Enum e) noexcept
+{
+    static_assert(std::is_enum_v<Enum>, "to_underlying must be passed an enum");
+    return static_cast<std::underlying_type_t<Enum>>(e);
+}
+
 } // namespace nw
