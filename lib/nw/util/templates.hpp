252e7fd3a290535beb3ea161f9745e19f4aa71e6
jmd <joshua.m.dean@gmail.com>
2022-02-08 14:49:11 -0800

    Add template for creating reverse wrapper
    Handy for range based for loops

--- lib/nw/util/templates.hpp
+++ lib/nw/util/templates.hpp
@@ -13,4 +13,19 @@ constexpr std::underlying_type_t<Enum> to_underlying(Enum e) noexcept
     return static_cast<std::underlying_type_t<Enum>>(e);
 }
 
+// From https://stackoverflow.com/questions/8542591/c11-reverse-range-based-for-loop
+template <typename T>
+struct reversion_wrapper {
+    T& iterable;
+};
+
+template <typename T>
+auto begin(reversion_wrapper<T> w) { return std::rbegin(w.iterable); }
+
+template <typename T>
+auto end(reversion_wrapper<T> w) { return std::rend(w.iterable); }
+
+template <typename T>
+reversion_wrapper<T> reverse(T&& iterable) { return {iterable}; }
+
 } // namespace nw
