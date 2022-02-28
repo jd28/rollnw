6ef3cdfd28f459834cc51d0b87ece707950602a4
jmd <joshua.m.dean@gmail.com>
2022-02-28 06:40:28 -0800

    Add function for creating unique tmp directories.

--- lib/nw/util/platform.cpp
+++ lib/nw/util/platform.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <nowide/cstdlib.hpp>
 
+#include <random>
+#include <sstream>
+
 namespace nw {
 
 NWNPaths get_nwn_paths()
@@ -19,6 +22,26 @@ NWNPaths get_nwn_paths()
     }
 
     return result;
+}
+
+std::filesystem::path create_unique_tmp_path()
+{
+    static thread_local std::random_device dev;
+    static thread_local std::mt19937 prng(dev());
+
+    auto tmp_dir = std::filesystem::temp_directory_path();
+    uint64_t i = 0;
+    std::uniform_int_distribution<uint64_t> rand(0);
+    std::filesystem::path path;
+    while (true) {
+        std::stringstream ss;
+        ss << std::hex << rand(prng);
+        path = tmp_dir / ss.str();
+        if (std::filesystem::create_directory(path) || ++i > 100) {
+            break;
+        }
+    }
+    return path;
 }
 
 } // namespace nw
--- lib/nw/util/platform.hpp
+++ lib/nw/util/platform.hpp
@@ -13,4 +13,7 @@ struct NWNPaths {
 /// WARNING - HUGE PUNT - JUST READS ENV VARS ``NWN_ROOT`` and ``NWN_HOME``
 NWNPaths get_nwn_paths();
 
+/// Creates randomly named folder in tmp.  Analguous to POSIX `mkdtemp`.
+std::filesystem::path create_unique_tmp_path();
+
 } // namespace nw
