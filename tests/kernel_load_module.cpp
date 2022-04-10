c4eca3dc0ec01fda8f82aa1f5a9a1e32274f3ad0
jmd <joshua.m.dean@gmail.com>
2022-04-09 19:35:21 -0700

    Add some more tests to make sure loading module from dir or zip works
    Also make sure some basic instantiation works

--- tests/kernel_load_module.cpp
+++ tests/kernel_load_module.cpp
@@ -7,7 +7,7 @@
 
 using namespace std::literals;
 
-TEST_CASE("load module", "[kernel]")
+TEST_CASE("load module from .mod", "[kernel]")
 {
     auto mod = nw::kernel::load_module("test_data/user/modules/DockerDemo.mod");
     REQUIRE(mod);
@@ -16,6 +16,36 @@ TEST_CASE("load module", "[kernel]")
     REQUIRE(area.common()->resref == "test_area");
     REQUIRE(area.creatures.size() > 0);
     REQUIRE(area.creatures[0]->hp_max == 110);
+
+    nw::kernel::unload_module();
+}
+
+TEST_CASE("load module from directory", "[kernel]")
+{
+    auto mod = nw::kernel::load_module("test_data/user/modules/module_as_dir/");
+    REQUIRE(mod);
+    REQUIRE(mod->area_count() == 1);
+    nw::Area& area = *mod->get_area(0);
+    REQUIRE(area.common()->resref == "test_area");
+    REQUIRE(area.creatures.size() > 0);
+    REQUIRE(area.creatures[0]->hp_max == 110);
+    auto cre = nw::kernel::objects().load_as<nw::Creature>("test_creature");
+    REQUIRE(cre);
+
+    nw::kernel::unload_module();
+}
+
+TEST_CASE("load module from .zip", "[kernel]")
+{
+    auto mod = nw::kernel::load_module("test_data/user/modules/module_as_zip.zip");
+    REQUIRE(mod);
+    REQUIRE(mod->area_count() == 1);
+    nw::Area& area = *mod->get_area(0);
+    REQUIRE(area.common()->resref == "test_area");
+    REQUIRE(area.creatures.size() > 0);
+    REQUIRE(area.creatures[0]->hp_max == 110);
+    auto cre = nw::kernel::objects().load_as<nw::Creature>("test_creature");
+    REQUIRE(cre);
 
     nw::kernel::unload_module();
 }
