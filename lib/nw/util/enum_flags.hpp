4723b8acd54a2549c302100d09828dd8c9987c6f
jmd <joshua.m.dean@gmail.com>
2022-04-23 09:18:15 -0700

    Update DEFINE_ENUM_FLAGS to use to_underlying

--- lib/nw/util/enum_flags.hpp
+++ lib/nw/util/enum_flags.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
-#define DEFINE_ENUM_FLAGS(T)                                                                                                                       \
-    inline T operator&(T x, T y) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) & static_cast<std::underlying_type_t<T>>(y)); } \
-    inline T operator|(T x, T y) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) | static_cast<std::underlying_type_t<T>>(y)); } \
-    inline T operator^(T x, T y) { return static_cast<T>(static_cast<std::underlying_type_t<T>>(x) ^ static_cast<std::underlying_type_t<T>>(y)); } \
-    inline T operator~(T x) { return static_cast<T>(~static_cast<std::underlying_type_t<T>>(x)); }                                                 \
-    inline T& operator&=(T& x, T y) { return x = (x & y); }                                                                                        \
-    inline T& operator|=(T& x, T y) { return x = (x | y); }                                                                                        \
-    inline T& operator^=(T& x, T y) { return x = (x ^ y); }                                                                                        \
-    inline bool operator!(T x) { return !static_cast<std::underlying_type_t<T>>(x); }
+#include "templates.hpp"
+
+#define DEFINE_ENUM_FLAGS(T)                                                                     \
+    inline T operator&(T x, T y) { return static_cast<T>(to_underlying(x) & to_underlying(y)); } \
+    inline T operator|(T x, T y) { return static_cast<T>(to_underlying(x) | to_underlying(y)); } \
+    inline T operator^(T x, T y) { return static_cast<T>(to_underlying(x) ^ to_underlying(y)); } \
+    inline T operator~(T x) { return static_cast<T>(~to_underlying(x)); }                        \
+    inline T& operator&=(T& x, T y) { return x = (x & y); }                                      \
+    inline T& operator|=(T& x, T y) { return x = (x | y); }                                      \
+    inline T& operator^=(T& x, T y) { return x = (x ^ y); }                                      \
+    inline bool operator!(T x) { return !to_underlying(x); }
