08a330bd5eb3072dd11fc5b201d899a58c81fffa
jmd <joshua.m.dean@gmail.com>
2022-06-11 20:34:53 -0700

    chore: cleanup benchmarks.. kind of

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -2,7 +2,9 @@
 #include <nw/i18n/Tlk.hpp>
 #include <nw/kernel/Kernel.hpp>
 #include <nw/objects/Creature.hpp>
+#include <nw/profiles/nwn1/Profile.hpp>
 #include <nw/serialization/GffInputArchive.hpp>
+#include <toml++/toml.hpp>
 
 #include <benchmark/benchmark.h>
 #include <nlohmann/json.hpp>
@@ -10,22 +12,31 @@
 
 #include <fstream>
 
+namespace fs = std::filesystem;
+
 static void BM_parse_feat_2da(benchmark::State& state)
 {
-    nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/development/feat.2da");
+    nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/development/placeables.2da");
     for (auto _ : state) {
         nw::TwoDA tda{ba};
         benchmark::DoNotOptimize(tda);
     }
 }
 
+static void BM_parse_settings_tml(benchmark::State& state)
+{
+    nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/settings.tml");
+    for (auto _ : state) {
+        auto out = toml::parse(ba.string_view());
+        benchmark::DoNotOptimize(out);
+    }
+}
+
 static void BM_creature_from_gff(benchmark::State& state)
 {
     for (auto _ : state) {
-        nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/development/pl_agent_001.utc");
-        nw::GffInputArchive in{ba};
-        nw::Creature cre{in.toplevel(), nw::SerializationProfile::blueprint};
-        benchmark::DoNotOptimize(cre);
+        auto ent = nw::kernel::objects().load(fs::path("../tests/test_data/user/development/pl_agent_001.utc"));
+        benchmark::DoNotOptimize(ent);
     }
 }
 
@@ -33,39 +44,47 @@ static void BM_creature_from_json(benchmark::State& state)
 {
     nlohmann::json j;
     for (auto _ : state) {
-        std::ifstream f{"../tests/test_data/user/development/pl_agent_001.utc.json", std::ifstream::binary};
-        j = nlohmann::json::parse(f);
-        nw::Creature cre{j, nw::SerializationProfile::blueprint};
-        benchmark::DoNotOptimize(cre);
+        auto ent = nw::kernel::objects().load(fs::path("../tests/test_data/user/development/pl_agent_001.utc.json"));
+        benchmark::DoNotOptimize(ent);
     }
 }
 
 static void BM_creature_to_json(benchmark::State& state)
 {
-    nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/development/pl_agent_001.utc");
-    nw::GffInputArchive in{ba};
-    nw::Creature cre{in.toplevel(), nw::SerializationProfile::blueprint};
+    auto ent = nw::kernel::objects().load(fs::path("../tests/test_data/user/development/pl_agent_001.utc"));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(cre.to_json(nw::SerializationProfile::instance));
+        nlohmann::json j;
+        benchmark::DoNotOptimize(nw::Creature::serialize(ent, j, nw::SerializationProfile::instance));
     }
 }
 
 static void BM_creature_to_gff(benchmark::State& state)
 {
-    nw::ByteArray ba = nw::ByteArray::from_file("../tests/test_data/user/development/pl_agent_001.utc");
-    nw::GffInputArchive in{ba};
-    nw::Creature cre{in.toplevel(), nw::SerializationProfile::blueprint};
+    auto ent = nw::kernel::objects().load(fs::path("../tests/test_data/user/development/pl_agent_001.utc"));
     for (auto _ : state) {
-        auto out = cre.to_gff(nw::SerializationProfile::blueprint);
+        auto out = nw::Creature::serialize(ent, nw::SerializationProfile::instance);
+        benchmark::DoNotOptimize(out);
+    }
+}
+
+static void BM_creature_select(benchmark::State& state)
+{
+    auto ent = nw::kernel::objects().load(fs::path("../tests/test_data/user/development/pl_agent_001.utc"));
+    auto sel = nw::select::ability(nwn1::ability_strength);
+
+    for (auto _ : state) {
+        auto out = nw::kernel::rules().select(sel, ent);
         benchmark::DoNotOptimize(out);
     }
 }
 
 BENCHMARK(BM_parse_feat_2da);
+BENCHMARK(BM_parse_settings_tml);
 BENCHMARK(BM_creature_from_gff);
 BENCHMARK(BM_creature_from_json);
 BENCHMARK(BM_creature_to_gff);
 BENCHMARK(BM_creature_to_json);
+BENCHMARK(BM_creature_select);
 
 int main(int argc, char** argv)
 {
@@ -95,6 +114,7 @@ int main(int argc, char** argv)
     }
 
     nw::kernel::services().start();
+    nw::kernel::load_profile(new nwn1::Profile);
 
     ::benchmark::Initialize(&argc, argv);
     if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
