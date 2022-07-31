60683647086b5d94f7990a82ed3ed6b9ff2f40d8
jmd <joshua.m.dean@gmail.com>
2022-07-31 08:10:05 -0700

    Add some tests for nwscript lexer

--- tests/formats_nss.cpp
+++ tests/formats_nss.cpp
@@ -29,3 +29,23 @@ TEST_CASE("NWScript Parser - nwscript", "[formats]")
     script::Nss nss("test_data/user/scratch/nwscript.nss");
     REQUIRE_NOTHROW(nss.parse());
 }
+
+TEST_CASE("NWScript lexer", "[formats]")
+{
+    script::NssLexer lexer{"x /= y"};
+    REQUIRE(lexer.next().id == "x");
+    REQUIRE(lexer.next().id == "/=");
+    REQUIRE(lexer.next().id == "y");
+
+    script::NssLexer lexer2{"/*"};
+    REQUIRE_THROWS(lexer2.next());
+
+    script::NssLexer lexer3{"/* this is comment */"};
+    REQUIRE(lexer2.next().type == script::NssTokenType::END); // comments are skipped currently
+
+    script::NssLexer lexer4{"\"this is unterminated"};
+    REQUIRE_THROWS(lexer4.next());
+
+    script::NssLexer lexer5{"\"Hello World\""};
+    REQUIRE(lexer5.next().id == "Hello World");
+}
