7fd5b73c8b438a01a0a8c0409730c69cd400f331
jmd <joshua.m.dean@gmail.com>
2022-12-03 20:26:49 -0800

    [tests] remove TwoDACache tests from kernel_rules

--- tests/kernel_rules.cpp
+++ tests/kernel_rules.cpp
@@ -14,50 +14,6 @@
 
 namespace nwk = nw::kernel;
 
-TEST_CASE("rules manager", "[kernel]")
-{
-    auto mod = nwk::load_module("test_data/user/modules/DockerDemo.mod");
-    REQUIRE(mod);
-
-    //     auto* abs = nwk::world().get<nw::AbilityArray>();
-    //     REQUIRE(abs);
-    //     REQUIRE(abs->entries.size() == 6);
-
-    //     auto* ba = nwk::world().get<nw::BaseItemArray>();
-    //     REQUIRE(ba);
-    //     REQUIRE(ba->entries.size() > 0);
-
-    //     auto* cl = nwk::world().get<nw::ClassArray>();
-    //     REQUIRE(cl);
-    //     REQUIRE(cl->entries.size() > 0);
-
-    //     auto* ft = nwk::world().get<nw::FeatArray>();
-    //     REQUIRE(ft);
-    //     REQUIRE(ft->entries.size() > 0);
-
-    //     auto* ra = nwk::world().get<nw::RaceArray>();
-    //     REQUIRE(ra);
-    //     REQUIRE(ra->entries.size() > 0);
-
-    //     auto* sk = nwk::world().get<nw::SkillArray>();
-    //     REQUIRE(sk);
-    //     REQUIRE(sk->entries.size() > 0);
-
-    //     auto* sp = nwk::world().get<nw::SpellArray>();
-    //     REQUIRE(sp);
-    //     REQUIRE(sp->entries.size() > 0);
-
-    //     auto& skill = sk->entries[0];
-    //     REQUIRE(skill.valid());
-    //     REQUIRE(skill.name == 269);
-    //     REQUIRE(skill.constant == "SKILL_ANIMAL_EMPATHY");
-
-    REQUIRE(nwk::services().get_mut<nwk::TwoDACache>()->cache("placeables"sv));
-    REQUIRE(nwk::services().get_mut<nwk::TwoDACache>()->get("placeables"sv)->rows() > 0);
-
-    nw::kernel::unload_module();
-}
-
 TEST_CASE("rules system: class info", "[kernel]")
 {
     auto mod = nwk::load_module("test_data/user/modules/DockerDemo.mod");
