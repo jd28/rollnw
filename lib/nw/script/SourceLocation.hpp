c7ba663da9a932c40bb487c279b48a2c4d978325
jmd <joshua.m.dean@gmail.com>
2023-11-23 22:16:37 -0800

    [script] fix: Flag SourceLocation functions as noexcept

--- lib/nw/script/SourceLocation.hpp
+++ lib/nw/script/SourceLocation.hpp
@@ -23,14 +23,14 @@ struct SourceLocation {
     SourcePosition position;     ///< Position
 
     /// Gets the length of source code covered
-    size_t length() const
+    size_t length() const noexcept
     {
         if (!start || !end) { return {}; }
         return size_t(end - start);
     }
 
     /// Gets a view of the source code covered
-    std::string_view view() const
+    std::string_view view() const noexcept
     {
         if (!start || !end) { return {}; }
         return std::string_view{start, length()};
