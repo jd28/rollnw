eeefd23bd038476b84cf67c4d8946fc7b6d91c09
jmd <joshua.m.dean@gmail.com>
2024-02-20 20:18:22 -0800

    [profile] fix: delete unused containers vars

--- lib/nw/profiles/nwn1/Profile.hpp
+++ lib/nw/profiles/nwn1/Profile.hpp
@@ -4,9 +4,6 @@
 #include "rules.hpp"
 
 #include "../../kernel/GameProfile.hpp"
-#include "../../resources/Directory.hpp"
-#include "../../resources/Erf.hpp"
-#include "../../resources/Key.hpp"
 
 namespace nwn1 {
 
@@ -26,21 +23,8 @@ struct Profile : nw::GameProfile {
      */
     bool load_rules() const override;
 
+    /// Loads containers into resman
     bool load_resources() override;
-
-    nw::Directory ambient_user_, ambient_install_;
-    nw::Directory development_;
-    nw::Directory dmvault_user_, dmvault_install_;
-    nw::Directory localvault_user_, localvault_install_;
-    nw::Directory music_user_, music_install_;
-    nw::Directory override_user_, override_install_;
-    nw::Directory portraits_user_, portraits_install_;
-    nw::Directory servervault_user_, servervault_install_;
-
-    std::vector<nw::unique_container> patches_;
-
-    std::vector<nw::Erf> texture_packs_;
-    std::vector<nw::Key> keys_;
 };
 
 } // namespace nwn1
