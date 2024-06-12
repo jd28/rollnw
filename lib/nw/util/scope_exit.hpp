be320874a098aca1e70de8ce50ccf9fdc9ed9ad7
jmd <joshua.m.dean@gmail.com>
2024-06-11 17:06:01 -0700

    [util] fix: move scope_exit out of namespace nw

--- lib/nw/util/scope_exit.hpp
+++ lib/nw/util/scope_exit.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-namespace nw {
-
 // Remove if ever added to standard.  This is basically from boost.
 
 template <typename T>
@@ -33,8 +31,6 @@ scope_exit<T> create_scope_exit(T&& exit)
 {
     return scope_exit<T>(std::forward<T>(exit));
 }
-
-} // namespace nw
 
 /// @private
 #define _EXIT_SCOPE_LINENAME_CAT(name, line) name##line
