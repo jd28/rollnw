869a1e8b1ecd622de6a2870ac2fa0e19d8658a59
jmd <joshua.m.dean@gmail.com>
2024-10-26 20:31:38 -0700

    [tests] add: rollnw_test_all_2da to test all default 2das

---
+++ tests_ext/all_2da/main.cpp
@@ -0,0 +1,47 @@
+#include <nw/formats/StaticTwoDA.hpp>
+#include <nw/kernel/Kernel.hpp>
+#include <nw/kernel/Resources.hpp>
+#include <nwn1/Profile.hpp>
+
+#include <nowide/cstdlib.hpp>
+
+#include <chrono>
+
+int main(int argc, char* argv[])
+{
+    nw::init_logger(argc, argv);
+
+    nw::kernel::config().set_paths("", "test_data/user/");
+    nw::kernel::config().initialize();
+    nw::kernel::services().start();
+
+    std::string biggest_name;
+    int64_t biggest = 0;
+    int64_t sum = 0;
+    int count = 0;
+    size_t invalid = 0;
+
+    auto callback = [&](const nw::Resource& res) {
+        if (res.type != nw::ResourceType::twoda) { return; }
+        auto start = std::chrono::high_resolution_clock::now();
+        nw::StaticTwoDA tda{nw::kernel::resman().demand(res)};
+        if (!tda.is_valid()) {
+            ++invalid;
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto total = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+        if (total > biggest) {
+            biggest = total;
+            biggest_name = res.filename();
+        }
+        sum += total;
+        ++count;
+    };
+
+    nw::kernel::resman().visit(callback);
+
+    LOG_F(INFO, "processing all default {} 2da, {} invalid", count, invalid);
+    LOG_F(INFO, "  total: {}ns, avg processing time: {}ns, longest: {}ns, longest name: {}", sum, sum / count, biggest, biggest_name);
+
+    return 0;
+}
