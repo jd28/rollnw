cd791efd01f23db663a9507f52dd5950b22568d6
jmd <joshua.m.dean@gmail.com>
2025-01-06 12:26:56 -0800

    [util] add: some helper macros for early return, supposedly that's bad
    oh well

--- lib/nw/util/macros.hpp
+++ lib/nw/util/macros.hpp
@@ -5,3 +5,27 @@
 
 /// Silences unused variable warnings
 #define ROLLNW_UNUSED(thing) (void)thing
+
+#define ENSURE_OR_RETURN(cond, fmt, ...)      \
+    do {                                      \
+        if (!(cond)) {                        \
+            LOG_F(ERROR, fmt, ##__VA_ARGS__); \
+            return;                           \
+        }                                     \
+    } while (0)
+
+#define ENSURE_OR_RETURN_ZERO(cond, fmt, ...) \
+    do {                                      \
+        if (!(cond)) {                        \
+            LOG_F(ERROR, fmt, ##__VA_ARGS__); \
+            return 0;                         \
+        }                                     \
+    } while (0)
+
+#define ENSURE_OR_RETURN_FALSE(cond, fmt, ...) \
+    do {                                       \
+        if (!(cond)) {                         \
+            LOG_F(ERROR, fmt, ##__VA_ARGS__);  \
+            return false;                      \
+        }                                      \
+    } while (0)
