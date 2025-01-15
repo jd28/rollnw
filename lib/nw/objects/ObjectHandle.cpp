38b6e27a97b580dd1e454cf812e53c4bb7aa5d75
jmd <joshua.m.dean@gmail.com>
2025-01-14 19:19:36 -0800

    [objects] convert ObjectType to u8
    [objects] add ObjectHandle::to_ull

--- lib/nw/objects/ObjectHandle.cpp
+++ lib/nw/objects/ObjectHandle.cpp
@@ -22,12 +22,20 @@ void to_json(nlohmann::json& j, ObjectID id)
 
 void from_json(const nlohmann::json& j, ObjectType& type)
 {
-    type = static_cast<ObjectType>(j.get<uint32_t>());
+    type = static_cast<ObjectType>(j.get<uint8_t>());
 }
 
 void to_json(nlohmann::json& j, ObjectType type)
 {
-    j = static_cast<uint32_t>(type);
+    j = static_cast<uint8_t>(type);
+}
+
+// == ObjectHandle ============================================================
+// ============================================================================
+
+uint64_t ObjectHandle::to_ull() const noexcept
+{
+    return (static_cast<uint64_t>(type) << 32) | static_cast<uint64_t>(id);
 }
 
 } // namespace nw
--- lib/nw/objects/ObjectHandle.hpp
+++ lib/nw/objects/ObjectHandle.hpp
@@ -6,8 +6,8 @@
 
 namespace nw {
 
-// -- ObjectID ----------------------------------------------------------------
-//-----------------------------------------------------------------------------
+// == ObjectID ================================================================
+// ============================================================================
 
 /// Opaque type.. for now.
 enum class ObjectID : uint32_t {};
@@ -21,12 +21,12 @@ void from_json(const nlohmann::json& j, ObjectID& id);
 /// nlohmann::json specialization
 void to_json(nlohmann::json& j, ObjectID id);
 
-// -- ObjectType --------------------------------------------------------------
-//-----------------------------------------------------------------------------
+// == ObjectType ==============================================================
+// ============================================================================
 
 /// Object types
-enum struct ObjectType : uint16_t {
-    invalid = 0, // Not real
+enum struct ObjectType : uint8_t {
+    invalid = 0,
     gui = 1,
     tile = 2,
     module = 3,
@@ -52,15 +52,21 @@ void from_json(const nlohmann::json& j, ObjectType& type);
 /// nlohmann::json specialization
 void to_json(nlohmann::json& j, ObjectType type);
 
-// -- ObjectHandle ------------------------------------------------------------
-//-----------------------------------------------------------------------------
+// == ObjectHandle ============================================================
+// ============================================================================
 
+/// Basic means of referring to an object
+/// @note In case of binding to languages with only doubles, actual data must
+/// be less than 2^53 - 1.
 struct ObjectHandle {
     ObjectID id = object_invalid;
     ObjectType type = ObjectType::invalid;
 
     bool operator==(const ObjectHandle&) const = default;
     std::strong_ordering operator<=>(const ObjectHandle& other) const = default;
+
+    /// Converst underlying data to unignsed 64 bit integer
+    uint64_t to_ull() const noexcept;
 };
 
 } // namespace nw
