bacd5e4496ca8b1b6ded8ae47880d4d5b04ae895
jmd <joshua.m.dean@gmail.com>
2025-05-31 07:59:12 -0700

    [util] add: random access iterator to ChunkVector
    -[util] change: block management from linked list to abseil InlinedVector in ChunkVector and allow forwarding inline vector size.

--- lib/nw/util/ChunkVector.hpp
+++ lib/nw/util/ChunkVector.hpp
@@ -5,12 +5,15 @@
 
 #include <memory>
 
+#include <absl/container/inlined_vector.h>
+
 namespace nw {
 
 /// ChunkVector is an intrusively linked list of chunks of arrays.
-template <typename T>
+template <typename T, size_t N = 8>
 struct ChunkVector {
     using reference = T&;
+    using const_reference = const T&;
 
     ChunkVector(size_t chunk_size, Allocator<T> allocator)
         : chunk_size_(chunk_size)
@@ -21,22 +24,19 @@ struct ChunkVector {
     ~ChunkVector()
     {
         clear();
-        auto c = blocks_;
-        while (c) {
-            std::allocator_traits<Allocator<T>>::deallocate(allocator_, c->data, chunk_size_);
-            auto temp = c;
-            c = c->next;
+        for (auto* it : blocks_) {
+            std::allocator_traits<Allocator<T>>::deallocate(allocator_, it->data, chunk_size_);
             auto ta = Allocator<Chunk>(allocator_);
-            std::allocator_traits<Allocator<Chunk>>::deallocate(ta, temp, 1);
+            std::allocator_traits<Allocator<Chunk>>::deallocate(ta, it, 1);
         }
     }
 
-    T& operator[](size_t index)
+    reference operator[](size_t index)
     {
         return const_cast<T&>(static_cast<const ChunkVector&>(*this)[index]);
     }
 
-    const T& operator[](size_t index) const
+    const_reference operator[](size_t index) const
     {
         auto chunk = find_chunk(index);
         size_t ele = index % chunk_size_;
@@ -61,24 +61,22 @@ struct ChunkVector {
     /// Clears the container
     void clear()
     {
-        auto c = blocks_;
         size_t total_size = 0;
-        while (c) {
+        for (auto* it : blocks_) {
             if constexpr (!std::is_trivially_destructible_v<T>) {
                 for (size_t i = 0; i < chunk_size_; ++i) {
                     if (total_size == size_) { break; }
-                    c->data[i].~T();
+                    it->data[i].~T();
                     ++total_size;
                 }
             }
-            c = c->next;
         }
         size_ = 0;
     }
 
     void pop_back()
     {
-        if (!blocks_) { return; }
+        if (blocks_.empty()) { return; }
         auto chunk = find_chunk(size_ - 1);
         CHECK_F(!!chunk && !!chunk->data, "attempting to address invalid chunk");
         chunk->data[(size_ - 1) % chunk_size_].~T();
@@ -87,7 +85,7 @@ struct ChunkVector {
 
     void push_back(T ele)
     {
-        if (!blocks_ || size_ == allocated_) { alloc_block(); }
+        if (blocks_.empty() || size_ == allocated_) { alloc_block(); }
 
         auto chunk = find_chunk(size_);
         CHECK_F(!!chunk && !!chunk->data, "attempting to address invalid chunk");
@@ -126,15 +124,28 @@ struct ChunkVector {
     /// Gets container size
     size_t size() const noexcept { return size_; }
 
+    template <bool IsConst>
+    struct Iterator;
+
+    using iterator = Iterator<false>;
+    using const_iterator = Iterator<true>;
+
+    iterator begin() { return iterator(this, 0); }
+    iterator end() { return iterator(this, size()); }
+
+    const_iterator begin() const { return const_iterator(this, 0); }
+    const_iterator end() const { return const_iterator(this, size()); }
+
+    const_iterator cbegin() const { return begin(); }
+    const_iterator cend() const { return end(); }
+
 private:
     struct Chunk {
         T* data = nullptr;
-        Chunk* next = nullptr;
     };
 
+    absl::InlinedVector<Chunk*, N> blocks_;
     size_t chunk_size_;
-    Chunk* blocks_ = nullptr;
-    Chunk* tail_ = nullptr;
     size_t size_ = 0;
     size_t allocated_ = 0;
     Allocator<T> allocator_;
@@ -144,31 +155,93 @@ private:
         auto ta = Allocator<Chunk>(allocator_);
         Chunk* chunk = std::allocator_traits<Allocator<Chunk>>::allocate(ta, 1);
         chunk->data = allocator_.allocate(chunk_size_);
-        chunk->next = nullptr;
-
-        if (!blocks_) {
-            blocks_ = tail_ = chunk;
-        } else {
-            tail_->next = chunk;
-            tail_ = chunk;
-        }
+        blocks_.push_back(chunk);
         allocated_ += chunk_size_;
     }
 
     Chunk* find_chunk(size_t index) const noexcept
     {
         size_t chunk = index / chunk_size_;
-        size_t i = 0;
-
-        auto c = blocks_;
-        while (c) {
-            if (i == chunk) { break; }
-            ++i;
-            c = c->next;
-        }
-
-        return c;
+        CHECK_F(chunk < blocks_.size(), "out of bounds");
+        return blocks_[chunk];
     }
+};
+
+template <typename T, size_t N>
+template <bool IsConst>
+struct ChunkVector<T, N>::Iterator {
+    using VecType = std::conditional_t<IsConst, const ChunkVector, ChunkVector>;
+    using value_type = std::conditional_t<IsConst, const T, T>;
+    using reference = value_type&;
+    using pointer = value_type*;
+    using difference_type = std::ptrdiff_t;
+    using iterator_category = std::random_access_iterator_tag;
+
+    Iterator()
+        : vec_{nullptr}
+        , index_{0}
+    {
+    }
+
+    Iterator(VecType* vec, size_t index)
+        : vec_{vec}
+        , index_{index}
+    {
+    }
+
+    reference operator*() const { return (*vec_)[index_]; }
+    pointer operator->() const { return &(*vec_)[index_]; }
+
+    Iterator& operator++()
+    {
+        ++index_;
+        return *this;
+    }
+    Iterator operator++(int)
+    {
+        auto tmp = *this;
+        ++index_;
+        return tmp;
+    }
+
+    Iterator& operator--()
+    {
+        --index_;
+        return *this;
+    }
+    Iterator operator--(int)
+    {
+        auto tmp = *this;
+        --index_;
+        return tmp;
+    }
+
+    Iterator& operator+=(difference_type n)
+    {
+        index_ += n;
+        return *this;
+    }
+    Iterator& operator-=(difference_type n)
+    {
+        index_ -= n;
+        return *this;
+    }
+
+    Iterator operator+(difference_type n) const { return {vec_, index_ + n}; }
+    Iterator operator-(difference_type n) const { return {vec_, index_ - n}; }
+    difference_type operator-(const Iterator& other) const { return index_ - other.index_; }
+
+    reference operator[](difference_type n) const { return (*vec_)[index_ + n]; }
+
+    bool operator==(const Iterator& other) const { return index_ == other.index_; }
+    bool operator!=(const Iterator& other) const { return index_ != other.index_; }
+    bool operator<(const Iterator& other) const { return index_ < other.index_; }
+    bool operator>(const Iterator& other) const { return index_ > other.index_; }
+    bool operator<=(const Iterator& other) const { return index_ <= other.index_; }
+    bool operator>=(const Iterator& other) const { return index_ >= other.index_; }
+
+    VecType* vec_;
+    size_t index_;
 };
 
 } // namespace nw
