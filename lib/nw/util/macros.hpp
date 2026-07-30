9805b50496ea60f114bd270e248744147a08e6c4
jmd <joshua.m.dean@gmail.com>
2026-07-29 20:25:26 -0700

    Make the ENSURE_OR_* macros preprocessor-portable

--- lib/nw/util/macros.hpp
+++ lib/nw/util/macros.hpp
@@ -6,50 +6,55 @@
 /// Silences unused variable warnings
 #define ROLLNW_UNUSED(thing) (void)thing
 
-#define ENSURE_OR_RETURN(cond, fmt, ...)      \
-    do {                                      \
-        if (!(cond)) {                        \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__); \
-            return;                           \
-        }                                     \
+// The format string is part of __VA_ARGS__ rather than a named parameter, so
+// there is never a trailing comma to suppress. `, ##__VA_ARGS__` is a GNU
+// extension clang warns about, and `__VA_OPT__` needs /Zc:preprocessor on MSVC;
+// this needs neither. A message is still required, as it was before.
+
+#define ENSURE_OR_RETURN(cond, ...)      \
+    do {                                 \
+        if (!(cond)) {                   \
+            LOG_F(ERROR, __VA_ARGS__);   \
+            return;                      \
+        }                                \
     } while (0)
 
-#define ENSURE_OR_RETURN_ZERO(cond, fmt, ...) \
-    do {                                      \
-        if (!(cond)) {                        \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__); \
-            return 0;                         \
-        }                                     \
+#define ENSURE_OR_RETURN_ZERO(cond, ...) \
+    do {                                 \
+        if (!(cond)) {                   \
+            LOG_F(ERROR, __VA_ARGS__);   \
+            return 0;                    \
+        }                                \
     } while (0)
 
-#define ENSURE_OR_RETURN_NULLPTR(cond, fmt, ...) \
+#define ENSURE_OR_RETURN_NULLPTR(cond, ...) \
+    do {                                    \
+        if (!(cond)) {                      \
+            LOG_F(ERROR, __VA_ARGS__);      \
+            return 0;                       \
+        }                                   \
+    } while (0)
+
+#define ENSURE_OR_RETURN_FALSE(cond, ...) \
+    do {                                  \
+        if (!(cond)) {                    \
+            LOG_F(ERROR, __VA_ARGS__);    \
+            return false;                 \
+        }                                 \
+    } while (0)
+
+#define ENSURE_OR_RETURN_DEFAULT(cond, ...) \
+    do {                                    \
+        if (!(cond)) {                      \
+            LOG_F(ERROR, __VA_ARGS__);      \
+            return {};                      \
+        }                                   \
+    } while (0)
+
+#define ENSURE_OR_RETURN_VALUE(value, cond, ...) \
     do {                                         \
         if (!(cond)) {                           \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__);    \
-            return 0;                            \
+            LOG_F(ERROR, __VA_ARGS__);           \
+            return (value);                      \
         }                                        \
-    } while (0)
-
-#define ENSURE_OR_RETURN_FALSE(cond, fmt, ...) \
-    do {                                       \
-        if (!(cond)) {                         \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__);  \
-            return false;                      \
-        }                                      \
-    } while (0)
-
-#define ENSURE_OR_RETURN_DEFAULT(cond, fmt, ...) \
-    do {                                         \
-        if (!(cond)) {                           \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__);    \
-            return {};                           \
-        }                                        \
-    } while (0)
-
-#define ENSURE_OR_RETURN_VALUE(value, cond, fmt, ...) \
-    do {                                              \
-        if (!(cond)) {                                \
-            LOG_F(ERROR, fmt __VA_OPT__(,) __VA_ARGS__);         \
-            return (value);                           \
-        }                                             \
     } while (0)
